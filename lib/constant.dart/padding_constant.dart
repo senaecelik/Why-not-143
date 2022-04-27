d24de3104f1694d4982cc19edfd7fdec049262ab
Sena Çelik <senaecelik@gmail.com>
2022-04-27 23:20:05 +0300

    onBoard added
    introduction_screen paketi eklendi

---
+++ lib/constant.dart/padding_constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class PaddingConstant {
+  static PaddingConstant instance = PaddingConstant._init();
+
+  PaddingConstant._init();
+
+  EdgeInsets onBoardPadding = const EdgeInsets.symmetric(horizontal: 34);
+}
