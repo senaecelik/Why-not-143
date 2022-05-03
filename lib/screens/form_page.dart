18cc73c36b813c3e7e56e679e10aec3571846442
tomiece317 <60846011+tomiece317@users.noreply.github.com>
2022-05-04 00:14:43 +0300

    selama

--- lib/screens/form_page.dart
+++ lib/screens/form_page.dart
@@ -6,6 +6,6 @@ class FormPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text('selam')]));
+    return Scaffold(body: Column(children: [Text('selama')]));
   }
 }
