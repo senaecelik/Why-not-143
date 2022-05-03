025b14bef2816a77b8b67ef79d71ac8524245dcc
tomiece317 <60846011+tomiece317@users.noreply.github.com>
2022-05-04 00:17:01 +0300

    selammm ekledik

---
+++ lib/screens/form_page.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class FormPage extends StatelessWidget {
+  const FormPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [Text('selammm')]));
+  }
+}
