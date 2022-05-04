129a7bcd2526fae10a1fb5fc2361d34fbfb90a5f
tomiece317 <60846011+tomiece317@users.noreply.github.com>
2022-05-04 21:56:40 +0300

    form sayfası deneme

--- lib/screens/form_page.dart
+++ lib/screens/form_page.dart
@@ -1,11 +1,60 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class FormPage extends StatelessWidget {
   const FormPage({Key? key}) : super(key: key);
+  InAppWebViewController controller;
+  String url = "https://forms.gle/oCr7bv8Q37A56Kua6";
+  double progress = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text('selammm')]));
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+          child: Container(
+              child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            child: progress < 1.0
+                ? LinearProgressIndicator(value: progress)
+                : Container(),
+          ),
+          Expanded(
+              child: Container(
+            margin: const EdgeInsets.all(10.0),
+            decoration:
+                BoxDecoration(border: Border.all(color: Colors.amberAccent)),
+            child: InAppWebView(
+              initialUrl: url,
+              initialHeaders: {},
+              initialOptions: InAppWebViewGroupOptions(
+                  crossPlatform: InAppWebViewOptions(debuggingEnabled: True)),
+              onWebViewCreated: (InAppWebViewController) =>
+                  controller = WebViewController,
+              onLoadStart: (controller, url) {
+                setState(() {
+                  this.url = url;
+                });
+              },
+              onLoadStop: (controller, url) {
+                setState(() {
+                  this.url = url;
+                });
+              },
+              onProgressChanged: (controller, progress) {
+                SetState(() {
+                  this.progress = progress / 100;
+                });
+              },
+            ),
+          ))
+        ],
+      ))),
+    );
   }
 }
