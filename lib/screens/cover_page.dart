8256809131d60eabe4ae7e7df28c4198296b831b
Sena Çelik <senaecelik@gmail.com>
2022-05-05 20:05:47 +0300

    cover page düzeltildi

--- lib/screens/cover_page.dart
+++ lib/screens/cover_page.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
 import 'package:why_not_143_team/constant.dart/asset_path.dart';
 import 'package:why_not_143_team/constant.dart/color_constant.dart';
 import 'package:why_not_143_team/constant.dart/padding_constant.dart';
 import 'package:why_not_143_team/constant.dart/string.dart';
-import 'package:why_not_143_team/constant.dart/text_style.dart';
 import 'package:why_not_143_team/route/route_constant.dart';
 import 'package:why_not_143_team/widget/button_widget.dart';
 
-class CoverPage extends StatelessWidget {
+class CoverPage extends StatefulWidget {
   const CoverPage({Key? key}) : super(key: key);
 
   @override
+  State<CoverPage> createState() => _CoverPageState();
+}
+
+class _CoverPageState extends State<CoverPage> {
+  @override
+  void initState() {
+    initialization();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(child: _coverPageBody(context)),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(child: _coverPageBody(context)),
+      ),
     );
   }
 
@@ -28,18 +42,15 @@ class CoverPage extends StatelessWidget {
           height: MediaQuery.of(context).size.height,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _appName(),
-              _animatedLogo(),
-              _getStartedButton()
-            ],
+            children: [_appName(), _animatedLogo(), _getStartedButton()],
           ),
         )
       ],
     );
   }
 
-  SizedBox _animatedLogo() => SizedBox(child: Lottie.asset(AssetPath.instance.coverImage));
+  SizedBox _animatedLogo() =>
+      SizedBox(child: Lottie.asset(AssetPath.instance.coverImage));
 
   Flexible _getStartedButton() {
     return Flexible(
@@ -52,16 +63,10 @@ class CoverPage extends StatelessWidget {
 
   Expanded _appName() {
     return Expanded(
-      flex: 2,
-      child: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          Padding(
-              padding: PaddingConstant.instance.appNamePadding,
-              child: const _TitleWidget()),
-          Positioned(left: 40, child: Image.asset(AssetPath.instance.appLogo)),
-        ],
-      ),
+      flex: 1,
+      child: Padding(
+          padding: PaddingConstant.instance.appNamePadding,
+          child: const _TitleWidget()),
     );
   }
 }
@@ -73,10 +78,14 @@ class _TitleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      StringConstant.instance.coverTitle,
-      style: TextStyleConstant.instance.largeTitle
-          .copyWith(color: ColorConstant.instance.yankeBlue),
-    );
+    return Text(StringConstant.instance.coverTitle,
+        style: TextStyle(
+            fontFamily: 'Pacifico',
+            color: ColorConstant.instance.yankeBlue,
+            fontSize: 48.sp));
   }
+}
+
+void initialization() async {
+  FlutterNativeSplash.remove();
 }
