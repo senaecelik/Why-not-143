1438dd8312e5ffb9b958b13cfc2a83d58c5ba05d
Sena Çelik <senaecelik@gmail.com>
2022-05-21 19:50:04 +0300

    Update register_page_view_model.dart

--- lib/meta/view/auth/register/register_page_view_model.dart
+++ lib/meta/view/auth/register/register_page_view_model.dart
@@ -1,16 +1,38 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:stacked/stacked.dart';
 import 'package:why_not_143_team/core/services/firebase_auth_method.dart';
 
 class RegisterViewModel extends BaseViewModel {
-  void registerUser(BuildContext context, registerNameController,
-      registerEmailController, registerPasswordController) {
-    context.read<FirebaseAuthMethods>().signUpWithEmail(
-        displayName: registerNameController.text,
-        email: registerEmailController.text,
-        password: registerPasswordController.text,
-        context: context);
+  void registerUser(
+     context,
+      registerNameController,
+      registerEmailController,
+      registerPasswordController,) {
+    setBusy(true);
+    context
+        .read<FirebaseAuthMethods>()
+        .signUpWithEmail(
+            displayName: registerNameController.text,
+            email: registerEmailController.text,
+            password: registerPasswordController.text,
+            context: context)
+        .then((value) => {setBusy(false)});
     notifyListeners();
+  }
+
+  String? emailValidatorMethod(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Lütfen, boş bırakmayın';
+    }
+    if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
+      return 'Geçerli bir email adresi giriniz';
+    }
+    return null;
+  }
+
+  String? emptyValidation(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Lütfen, boş bırakmayın';
+    }
+    return null;
   }
 }
