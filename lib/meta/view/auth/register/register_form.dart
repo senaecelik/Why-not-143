f2f1f8482eb792f93116e2f4ca66ad39aa7a5eaa
Sena Çelik <senaecelik@gmail.com>
2022-05-21 19:49:59 +0300

    Update register_form.dart

--- lib/meta/view/auth/register/register_form.dart
+++ lib/meta/view/auth/register/register_form.dart
@@ -3,161 +3,168 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stacked_hooks/stacked_hooks.dart';
 import 'package:why_not_143_team/meta/helper/constant/color_constant.dart';
 import 'package:why_not_143_team/meta/helper/constant/string.dart';
-import 'package:why_not_143_team/meta/helper/constant/text_style.dart';
 import 'package:why_not_143_team/meta/helper/constant/padding_constant.dart';
 import 'package:why_not_143_team/meta/view/auth/register/register_page_view_model.dart';
+import 'package:why_not_143_team/meta/widget/custom_circular.dart';
+import 'package:why_not_143_team/meta/widget/form_text.dart';
+
 class UserRegisterForm extends HookViewModelWidget<RegisterViewModel> {
   const UserRegisterForm({Key? key}) : super(key: key);
 
   @override
-  Widget buildViewModelWidget(
-      BuildContext context, RegisterViewModel viewModel) {
+  Widget buildViewModelWidget(context, viewModel) {
+    final _formKey = GlobalKey<FormState>();
     final nameController = TextEditingController();
-    final emailController = TextEditingController();
+    final registerEmailController = TextEditingController();
     final passwordController = TextEditingController();
 
     return Form(
+        key: _formKey,
         child: Column(
-      children: [
-        nameText(),
-        nameTextField(nameController),
-        mailTextField(emailController),
-        passTextField(passwordController),
-        signupButton(viewModel, context, nameController, emailController,
-            passwordController),
-      ],
-    ));
+          children: [
+            FormText(text: StringConstant.instance.formregisterName),
+            nameTextField(context, nameController, viewModel),
+            FormText(text: StringConstant.instance.formEmail),
+            mailTextField(context, registerEmailController, viewModel),
+            FormText(text: StringConstant.instance.formPassword),
+            passTextField(context, passwordController, viewModel),
+            viewModel.isBusy
+                ? const CustomCircular()
+                : signupButton(
+                    _formKey,
+                    viewModel,
+                    context,
+                    nameController,
+                    registerEmailController,
+                    passwordController,
+                  )
+          ],
+        ));
   }
 
-  Padding passTextField(registerPasswordController) {
+  Padding phoneTextField(context, registerPhoneController, viewModel) {
     return Padding(
       padding: PaddingConstant.instance.loginPadding,
       child: SizedBox(
         height: 58.h,
-        width: 328.w,
-        child: TextField(
+        width: MediaQuery.of(context).size.height,
+        child: TextFormField(
+          controller: registerPhoneController,
+          decoration: InputDecoration(
+            hintText: StringConstant.instance.formTextFieldPhone,
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide:
+                    BorderSide(color: ColorConstant.instance.neutral300)),
+          ),
+          validator: (value) {
+            viewModel.emptyValidation(value);
+            return null;
+          },
+        ),
+      ),
+    );
+  }
+
+  Padding passTextField(context, registerPasswordController, viewModel) {
+    return Padding(
+      padding: PaddingConstant.instance.loginPadding,
+      child: SizedBox(
+        height: 58.h,
+        width: MediaQuery.of(context).size.height,
+        child: TextFormField(
           obscureText: true,
           controller: registerPasswordController,
           decoration: InputDecoration(
-            hintText: StringConstant.instance.loginTextFieldPassword,
+            hintText: StringConstant.instance.formTextFieldPassword,
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10),
                 borderSide:
                     BorderSide(color: ColorConstant.instance.neutral300)),
           ),
+          validator: (value) {
+            viewModel.emptyValidation(value);
+            return null;
+          },
         ),
       ),
     );
   }
 
-  Padding mailTextField(registerEmailController) {
+  Padding mailTextField(
+      context, registerEmailController, RegisterViewModel viewModel) {
     return Padding(
       padding: PaddingConstant.instance.loginPadding,
       child: SizedBox(
         height: 58.h,
-        width: 328.w,
-        child: TextField(
+        width: MediaQuery.of(context).size.height,
+        child: TextFormField(
           keyboardType: TextInputType.emailAddress,
           controller: registerEmailController,
           decoration: InputDecoration(
-            hintText: StringConstant.instance.loginTextFieldMail,
+            hintText: StringConstant.instance.formTextFieldMail,
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10),
                 borderSide:
                     BorderSide(color: ColorConstant.instance.neutral300)),
           ),
+          validator: (value) {
+            viewModel.emailValidatorMethod(value);
+            return null;
+          },
         ),
       ),
     );
   }
 
-  Padding nameTextField(registerNameController) {
+  Padding nameTextField(context, registerNameController, viewModel) {
     return Padding(
       padding: PaddingConstant.instance.loginPadding,
       child: SizedBox(
         height: 58.h,
-        width: 328.w,
-        child: TextField(
+        width: MediaQuery.of(context).size.height,
+        child: TextFormField(
           controller: registerNameController,
           decoration: InputDecoration(
-            hintText: StringConstant.instance.registerName,
+            hintText: StringConstant.instance.formregisterName,
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10),
                 borderSide:
                     BorderSide(color: ColorConstant.instance.neutral300)),
           ),
+          validator: (value) {
+            viewModel.emptyValidation(value);
+            return null;
+          },
         ),
       ),
     );
   }
 
-  Padding passwordText() {
+  Widget signupButton(_formKey, viewModel, context, nameController,
+      emailController, passwordController) {
     return Padding(
-      padding: PaddingConstant.instance.loginPadding,
-      child: Row(
-        children: [
-          Text(
-            StringConstant.instance.loginPassword,
-            style: TextStyleConstant.instance.textSmallMedium,
+        padding: PaddingConstant.instance.loginPadding,
+        child: SizedBox(
+          height: 58.h,
+          width: MediaQuery.of(context).size.height,
+          child: ElevatedButton(
+            onPressed: () async {
+              if (_formKey.currentState!.validate()) {
+                viewModel.registerUser(context, nameController, emailController,
+                    passwordController);
+              }
+            },
+            child: Text(StringConstant.instance.loginSignIn),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              primary: ColorConstant.instance.yankeBlue,
+              onPrimary: ColorConstant.instance.white,
+              side: BorderSide(
+                  width: 1.0, color: ColorConstant.instance.yankeBlue),
+            ),
           ),
-        ],
-      ),
-    );
-  }
-
-  Padding emailText() {
-    return Padding(
-      padding: PaddingConstant.instance.loginPadding,
-      child: Row(
-        children: [
-          Text(
-            StringConstant.instance.loginEmail,
-            style: TextStyleConstant.instance.textSmallMedium,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Padding nameText() {
-    return Padding(
-      padding: PaddingConstant.instance.loginPadding,
-      child: Row(
-        children: [
-          Text(
-            StringConstant.instance.registerName,
-            style: TextStyleConstant.instance.textSmallMedium,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget signupButton(
-      RegisterViewModel viewModel,
-      BuildContext context,
-      TextEditingController nameController,
-      TextEditingController emailController,
-      TextEditingController passwordController) {
-    return InkWell(
-      onTap: () {
-        viewModel.registerUser(
-            context, nameController, emailController, passwordController);
-      },
-      child: Container(
-        height: 58.h,
-        width: 320.w,
-        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: ColorConstant.instance.yankeBlue),
-        child: Text(
-          StringConstant.instance.loginSignIn,
-          style: TextStyleConstant.instance.textLargeMedium
-              .copyWith(color: ColorConstant.instance.white),
-          textAlign: TextAlign.center,
-        ),
-      ),
-    );
+        ));
   }
 }
