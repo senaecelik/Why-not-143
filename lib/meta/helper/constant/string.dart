2b1bbd889852f5a05b8b0ae6b64a1b20a05e71de
Selda Koçak <104314077+selsld@users.noreply.github.com>
2022-06-04 18:06:51 +0300

    updated constant strings

--- lib/meta/helper/constant/string.dart
+++ lib/meta/helper/constant/string.dart
@@ -11,13 +11,13 @@ class StringConstant {
   ///Cover Page
   String onBoardTitle1 = "Pati dostunu seç!";
   String onBoardSubTitle1 =
-      "Pati Dostum sayesinde sana en yakın barınaktaki patileri görerek, seçtiğin pati için sahiplenme veya koruyucu aile olma başvurusu yapabilirsin.";
+      "Pati Dostum sayesinde sana en yakın barınaktaki patileri görerek, seçtiğin pati için sahiplenme başvurusu yapabilirsin.";
   String onBoardTitle2 = "Bağış yap - Destek ol";
   String onBoardSubTitle2 =
-      "Pati dostlarımız desteğini bekliyor. Barınak hesaplarına nakdi yardım yapabilir veya güncel barınak ihtiyaçlarını  direkt barınaklara gönderebilirsin.";
+      "Pati dostlarımız desteğini bekliyor. Barınak hesaplarına nakdi/ayni yardım yapabilir; reklam izleyerek Pati'ne destek olabilirsin.";
   String onBoardTitle3 = "Pati Rehber ile öğren";
   String onBoardSubTitle3 =
-      "Sahiplendiğin pati dostunun bakımıyla ilgili ipuçlarını paylaştığımız Pati Rehber sayfasına bakmayı unutma :)";
+      "Pati dostunun bakımıyla ilgili ipuçlarını paylaştığımız Pati Rehber sayfasına bakmayı unutma :)";
   String onBoardPass = "Geç";
 
   //Menu
@@ -65,9 +65,9 @@ class StringConstant {
   String aboutUs = "Biz kimiz?";
   String contactUs = "İletişim";
   String aboutText =
-      "Pati dostlarımız ırk,cins, renk, cinsiyet farketmeden desteklerimizi bekliyor. Bizler de küçük dokunuşlarla büyük değişimleri destekleyen, yardımlaşmayı seven küçük bir topluluk olarak Pati Dostum uygulamasını geliştirerek Canlarımıza destek olmayı amaçlıyoruz. Sokaklardan kurtularak barınaklarda bakılan dostlarımızın fazlalığı onlarla birebir ilgilenilmesini zorlaştırıyor. Siz de yolculuğunuzda bir pati dost edinmeye karar verdiyseniz belediyelere ve derneklere bağlı barınaklarımızdan uygulamamızı kullanarak barınağa gitmeye gerek kalmadan pati dostunuzu sahiplenebilirsiniz. Böylece ne yazık ki tüketim çılgınlığı ve gösteriş büyüsü etkisinde kalmış olan petshoplar yerine barınakları destekleyebilir; bir Can’ın kurtarılmasına destek olabilirsiniz. Uygulamamız yalnızca pati dost sahiplendirmekle kalmıyor, aynı zamanda; hayvanlara alerjiniz varsa ya da herhangi bir nedenle evde hayvan besleyemiyor ancak pati dostlarımıza yardım etmek istiyorsanız sizlere “Koruyucu Aile” olma şansı da tanıyor. Koruyucu ailesi olduğunuz pati dostunuzun tüm bakımları sizde olmak koşuluyla dostunuza güvenle barınakta bakılıyor. İki seçenek de sizin için uygun değilse uygulamamız aynı zaman da barınak hesaplarını ve adreslerini paylaşarak pati dostlarımıza nakdi ve ayni yardımda bulunmanızı sağlıyor. Haydi Pati Dostu’n seni bekliyor. Bugün bir Can’ı kurtararak fark yaratmak istemez misin?";
+      "Pati dostlarımız ırk,cins, renk, cinsiyet farketmeden desteklerimizi bekliyor. Bizler de küçük dokunuşlarla büyük değişimleri destekleyen, yardımlaşmayı seven küçük bir topluluk olarak Pati Dostum uygulamasını geliştirerek Canlarımıza destek olmayı amaçlıyoruz. Sokaklardan kurtularak barınaklarda bakılan dostlarımızın fazlalığı onlarla birebir ilgilenilmesini zorlaştırıyor. Siz de yolculuğunuzda bir pati dost edinmeye karar verdiyseniz belediyelere ve derneklere bağlı barınaklarımızdan uygulamamızı kullanarak barınağa gitmeye gerek kalmadan pati dostunuzu sahiplenebilirsiniz. Böylece ne yazık ki tüketim çılgınlığı ve gösteriş büyüsü etkisinde kalmış olan petshoplar yerine barınakları destekleyebilir; bir Can’ın kurtarılmasına destek olabilirsiniz. Uygulamamız yalnızca pati dost sahiplendirmekle kalmıyor, aynı zamanda; hayvanlara alerjiniz varsa ya da herhangi bir nedenle evde hayvan besleyemiyor ancak pati dostlarımıza yardım etmek istiyorsanız sizlere “Koruyucu Aile” olma şansı da tanıyor. Koruyucu ailesi olduğunuz pati dostunuza “Bağış Yap” sayfamızla ayni/nakdi yardımın yanı sıra reklam izleyerek de destek olabilirsiniz. Haydi Pati Dostu’n seni bekliyor. Bugün bir Can’ı kurtararak fark yaratmak istemez misin?";
   String contactText =
-      "Bizimle fikirlerinizi paylaşmak, yardım ve öneride bulunmak, sizde küçük bir adımla büyük etkiler yaratmak isterseniz bize ulaşmaktan çekinmeyin.";
+      "Bizimle fikirlerinizi paylaşmak, yardım ve öneride bulunmak isterseniz bize ulaşmaktan çekinmeyin.";
 
   String emailUs = "patidostum.whynot143@gmail.com";
 
@@ -79,7 +79,7 @@ class StringConstant {
 
   // messeage
   String errorMess = "Hata çıktı";
-  String commingSoonMess = "Sabrın sonu selamettir.";
+  String commingSoonMess = "Çok yakında...";
   String loginMess = "Lütfen giriş yapınız.";
 
   //Home
@@ -103,8 +103,8 @@ class StringConstant {
   String blogOther = "Diğer Yazılar";
 
   //ComingSoon
-  String coming = "Coming";
-  String soon = "Soon";
+  String coming = "Çok";
+  String soon = "Yakında";
 
   String copyIban = "Iban Kopyalandı";
 
